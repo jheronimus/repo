295e471cd93b9a5e1942cd316efc461aad9c4bcf
jheronimus <jheronimus@users.noreply.github.com>
2026-08-07 01:24:18 +0300

    Remove accidental files

--- test_deps.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-apk add cmake g++ make curl-dev ffmpeg-dev freeimage-dev freetype-dev harfbuzz-dev icu-dev gettext-dev libgit2-dev pugixml-dev sdl2-dev alsa-lib-dev bluez-dev eudev-dev poppler-dev git
